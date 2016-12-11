fd0ebed32eea492a94cd398c9b8ef93d86e47202
kabiroberai <oberai.kabir@gmail.com>
2016-12-11 11:54:32 +0530

    Initial Commit

---
+++ InfoPortal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfoPortal
+//
+//  Created by Kabir Oberai on 11/12/16.
+//  Copyright © 2016 Kabir Oberai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
