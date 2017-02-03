42f123d5f84234a225a1375cb41b4bcb08627482
kabiroberai <oberai.kabir@gmail.com>
2017-02-03 17:19:22 +0530

    Add more info to the Update struct

--- InfoPortal/Update.swift
+++ InfoPortal/Update.swift
@@ -11,28 +11,43 @@ import SwiftyJSON
 struct Update {
 	
 	let postID: Int
-	let authorID: Int
-	let target: String
 	let title: String
 	let content: String
+	let timestamp: Date?
+	
+	let authorID: String
+	let author: String
+	
+	let target: String
 	let targetName: String
+	
+	let color: String
 	
 	init?(json: JSON) {
 		guard let rawPostID = json["id"].string,
 			let postID = Int(rawPostID),
-			let rawAuthorID = json["author_id"].string,
-			let authorID = Int(rawAuthorID),
+			let authorID = json["author_id"].string,
+			let author = json["author"].string,
 			let target = json["target"].string,
 			let title = json["title"].string,
 			let content = json["content"].string,
-			let targetName = json["target_name"].string
+			let targetName = json["target_name"].string,
+			let color = json["colour"].string,
+			let timestamp = json["timestamp"].string
 			else { return nil }
 		self.postID = postID
 		self.authorID = authorID
+		self.author = author
 		self.target = target
 		self.title = title
 		self.content = content
 		self.targetName = targetName
+		self.color = color
+		
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+		formatter.timeZone = TimeZone(abbreviation: "IST")
+		self.timestamp = formatter.date(from: timestamp)
 	}
 	
 }
